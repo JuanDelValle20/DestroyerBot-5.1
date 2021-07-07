23ad695a5936f5f0ca66bdf1f3007a5bae7c8fdc
JuanDelValle18 <jdelvalle585@gmail.com>
2021-07-07 15:55:48 -0600

    Create shantera.js

---
+++ src/shantera.js
@@ -0,0 +1,22 @@
+const shantera = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes*
+
+
+- todo bien
+- buenos dias
+- bot malo
+- gracias bot
+- hola
+- fua
+- infoBot
+
+
+
+_Ojito escribe tal y como esta en el mensaje_
+_Nuevas palabras próximamente_
+
+by JuandelValle`
+
+}
+
+exports.shantera = shantera
