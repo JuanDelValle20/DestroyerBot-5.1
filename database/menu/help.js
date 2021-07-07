aec55d6835a5a328f08e707caf03a2c4acf06372
JuanDelValle18 <jdelvalle585@gmail.com>
2021-07-07 15:52:26 -0600

    Create help.js

---
+++ database/menu/help.js
@@ -0,0 +1,51 @@
+const help = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${ownerBot}
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *MENU*
+  │
+  ├─ ❏ ${prefix}logomakermenu
+  ├─ ❏ ${prefix}imagemakermenu
+  ├─ ❏ ${prefix}stickermakermenu
+  ├─ ❏ ${prefix}searchmenu
+  ├─ ❏ ${prefix}educationmenu
+  ├─ ❏ ${prefix}kerangmenu
+  ├─ ❏ ${prefix}downloadermenu
+  ├─ ❏ ${prefix}mememenu
+  ├─ ❏ ${prefix}groupmenu
+  ├─ ❏ ${prefix}soundmenu
+  ├─ ❏ ${prefix}musicmenu
+  ├─ ❏ ${prefix}islammenu
+  ├─ ❏ ${prefix}stalkmenu
+  ├─ ❏ ${prefix}wibumenu
+  ├─ ❏ ${prefix}funmenu
+  ├─ ❏ ${prefix}todmenu
+  ├─ ❏ ${prefix}informationmenu
+  ├─ ❏ ${prefix}stayonscreenmenu
+  ├─ ❏ ${prefix}ownermenu
+  └─ ❏ ${prefix}othermenu
+◪ *OTHER*
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}qrcode
+  ├─ ❏ ${prefix}timer
+  ├─ ❏ ${prefix}fml
+  └─ ❏ ${prefix}fml2
+
+  ⌜ *Esto es solo el comienzo de una era totalmente nueva UwU* ⌟ 
+  ⌜ *Shanduy es el creador de la base de este bot, JuandelValle es el que modificó la base para adaptarla a los grupos donde pertenece* ⌟
+`
+}
+exports.help = help
