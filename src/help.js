cdca541e8d1344e41d4928033c8b3cb0f8ac606c
JuanDelValle18 <jdelvalle585@gmail.com>
2021-07-07 15:55:36 -0600

    Create help.js

---
+++ src/help.js
@@ -0,0 +1,86 @@
+const help = (prefix) => {
+	return `
+
+⌜ *DestroyerBot 🥵 by JuandelValle* ⌟  
+
+◉ *INFORMACION*
+   ○ Comando: ⌜ ${prefix} ⌟
+   ○ Editor: JuandelValle™ 
+
+
+◉ *NUEVO COMANDO*
+- ${prefix}shantera
+Interactua con el bot
+
+◉ *NUEVOS MENUS*
+- ${prefix}desmenu
+Descargar musica y videos de YT
+- ${prefix}version
+Conoce la versión de tu bot
+- ${prefix}welmenu
+Comandos de bienvedia a grupos
+
+◉ *PARA USAR EL BOT*
+Registrate con el comando ${prefix}registro y tu nombre
+
+◉ *RESUELVE TUS DUDAS*
+  ║
+  ╠ ○ ${prefix}Editor
+  ╚ Dudas o problemas aqui
+
+◉ *CREAR STICKERS*
+  ║
+  ╠ ○ ${prefix}sticker
+  ╠ ○ ${prefix}attp
+  ╠ Mas un texto corto
+  ╠ ○ ${prefix}stickergif
+  ╚ 6 segundos de video
+
+◉ *CONVERTIDORES*
+  ║
+  ╠ ○ ${prefix}toimg
+  ╠ De sticker a JPG
+  ╠ ○ ${prefix}tomp3
+  ╚ De video a MP3
+
+◉ *AUDIO*
+  ║
+  ╠ ○ ${prefix}idioma
+  ╚ ○ ${prefix}tts es (mas texto)
+
+◉ *OTROS*
+  ║
+  ╠ ○ ${prefix}wame
+  ╠ Link de Whatsapp
+  ╠ ○ ${prefix}qrcode
+  ╚ Coloca un texto
+
+◉ *GRUPOS*
+  ║
+  ╠ ○ ${prefix}closegc
+  ╠ Cerrar el grupo solo admins
+  ╠ ○ ${prefix}opengc
+  ╠ Abrir grupo solo admins
+  ╠ ○ ${prefix}kick o hakai
+  ╠ Eliminar a un miembro 
+  ╠ ○ ${prefix}promote
+  ╠ Dar admin a un miembro
+  ╠ ○ ${prefix}demote
+  ╠ Quitar el admin
+  ╠ ○ ${prefix}linkgc
+  ╠ Link del grupo
+  ╠ ○ ${prefix}todos
+  ╚ Nombra a todos los del grupo
+
+Para usar estas funciones el bot necesita admin
+  
+
+
+by JuandelValle
+
+⌜ *Esto es solo el comienzo de una era totalmente nueva UwU* ⌟ 
+⌜ *Shanduy es el creador de la base de este bot, JuandelValle es el que modificó la base para adaptarla a los grupos donde pertenece* ⌟ 
+`
+}
+
+exports.help = help
