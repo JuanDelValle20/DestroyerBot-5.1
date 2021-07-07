02ac19590b91973471397d5408a6b531c816098f
JuanDelValle18 <jdelvalle585@gmail.com>
2021-07-07 15:55:50 -0600

    Create version.js

---
+++ src/version.js
@@ -0,0 +1,36 @@
+const version = (prefix, pushname) => {
+    return `
+*DestroyerBot 🥵*
+
+*Actualizado:* 19 de junio de 2021
+*Versión actual:* 4.1.0
+*Ofrecida por:* JuandelValle™
+
+
+*NOVEDADES*
+
+Bot interactivo
+
+Actualizacion de la version @adiwajshing/baileys a la mas estable
+
+Nuevo motor WAConnection :)
+
+
+*INFORME*
+
+Si no funciona el comando *play checa el blog del bot y mira la version que poses. 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento.
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos.
+Si vez a alguien que robo mi bot comunicate conmigo.
+
+
+*BLOG DE DestroyerBot*
+
+https://github.com/JuandelValle20/DestroyerBot-4.1.0
+
+by JuandelValle
+`
+
+}
+
+exports.version = version
