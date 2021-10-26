2c58ba1544bb94e2d79435e06f77d9d4a23c13bb
JuanDelValle18 <86022076+JuanDelValle20@users.noreply.github.com>
2021-10-26 15:34:51 -0600

    Create banmenu.js

---
+++ src/banmenu.js
@@ -0,0 +1,32 @@
+const banmenu = (prefix, pushname) => {
+    return `*Banear El Uso Del Bot*
+    
+ _El bot necesita de admin_
+
+Este comando impide que el baneado use el bot
+
+══════════════
+
+*PARA BANEAR*
+
+*ban + a la persona que deseas banear
+
+Ejemplo: *ban @xxxxxxx
+
+══════════════
+
+*PARA DESBANEAR*
+
+*desban + a la persona que deseas banear
+
+Ejemplo: *desban @xxxxxxx
+
+══════════════
+
+ву Juan del Valle
+
+`
+
+}
+
+exports.banmenu = banmenu
