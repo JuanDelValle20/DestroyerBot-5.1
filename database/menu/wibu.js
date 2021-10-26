341deb04f4c4901a4fb8fdca420b65f1ca31a02f
JuanDelValle18 <86022076+JuanDelValle20@users.noreply.github.com>
2021-10-26 15:35:41 -0600

    Update wibu.js

--- database/menu/wibu.js
+++ database/menu/wibu.js
@@ -16,6 +16,8 @@ const wibu = (prefix, ownerBot, botName) => {
   │
   ├─ ❏ ${prefix}neonime
   ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
   ├─ ❏ ${prefix}randomanime
   ├─ ❏ ${prefix}husbu
   ├─ ❏ ${prefix}husbu2
