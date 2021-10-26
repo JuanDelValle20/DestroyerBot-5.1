5b810227f87cebbd9d7d654deae57bb5934fd8d0
JuanDelValle18 <86022076+JuanDelValle20@users.noreply.github.com>
2021-10-26 15:35:13 -0600

    Create juegos.js

---
+++ src/juegos.js
@@ -0,0 +1,15 @@
+const juegos = (prefix, pushname) => {
+    return `
+*Comandos De Juegos 👾*
+ 
+
+${prefix}fan
+Prueba tu suerte en el medidor de fan de fan 😱
+
+_*En futuras versiones colocare mas juegos :)*_
+     
+ву Juan del Valle`
+
+}
+
+exports.juegos = juegos
