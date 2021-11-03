d35c6784925c7afe8317d06c7fd177bf525bbc0c
JuanDelValle18 <86022076+JuanDelValle20@users.noreply.github.com>
2021-11-03 13:09:21 -0600

    Update version.js

--- src/version.js
+++ src/version.js
@@ -2,14 +2,11 @@ const version = (prefix, pushname) => {
     return `
     *DestroyerBot ву Juan del Valle*
 
-    *Actualizado:* 01 de Noviembre del 2021
+    *Actualizado:* 03 de Noviembre del 2021
     *Versión actual:* 10.0
     *Ofrecida por:* Juan del Valle™
 
 
-*COMUNICADO*
-
-La funcion *kick sigue en mantenimiento
 
 
 _*by Juan del Valle*_
